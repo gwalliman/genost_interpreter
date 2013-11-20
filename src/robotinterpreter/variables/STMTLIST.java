a0ac8cdec99f90a1faee37145294396f7198ca50
Garret <gwallima@asu.edu>
2013-11-19 23:28:08 -0700

    More commenting.

--- src/robotinterpreter/variables/STMTLIST.java
+++ src/robotinterpreter/variables/STMTLIST.java
@@ -3,23 +3,45 @@ package robotinterpreter.variables;
 import robotinterpreter.Code;
 import robotinterpreter.RobotInterpreter;
 
+/**
+ * class STMTLIST
+ * 
+ * A STMTLIST is simply a linked-list interpretation for statements.
+ * Each STMTLIST node is one node in the linked list and has a reference to its consitituant STMT, along with various helper functions.
+ * 
+ * @author Garret Walliman (gwallima@asu.edu)
+ *
+ */
 public class STMTLIST extends Variable
 {
 	private STMT stmt;
+	//This is null at the end of the list
 	private STMTLIST nextStmt;
 	
+	/**
+	 * public STMTLIST(BODY b, Code c)
+	 * 
+	 * Gets the next STMT, parses it, and moves on to the next one, if it exists.
+	 * 
+	 * @param b	the parent body
+	 * @param c	the code object
+	 */
 	public STMTLIST(BODY b, Code c) 
 	{
 		body = b;
+		//Skip over any empty lines (i.e. whitespace only)
 		while(c.currentLine().trim().length() == 0) 
 		{ 
 			c.nextLine();
 		}
 		
+		//If we are not at the end of the parent body, make a new STMT
 		if(c.currentLineNum() != body.getFinishLine())
 			stmt = new STMT(body, c);
 		
 		c.nextLine();
+		
+		//If we are not at the end of the parent body or the end of the code, itself, add a new STMTLIST to the linked list
 		if(c.currentLine() != null && c.currentLineNum() != body.getFinishLine())
 		{
 			nextStmt = new STMTLIST(body, c);
@@ -27,16 +49,31 @@ public class STMTLIST extends Variable
 		else nextStmt = null;
 	}
 	
+	/**
+	 * public STMT getStmt()
+	 *
+	 * @return	the STMT for this STMTLIST
+	 */
 	public STMT getStmt() 
 	{
 		return stmt;
 	}
 	
+	/**
+	 * public STMTLIST getNextStmt()
+	 * 
+	 * @return	returns the next statement in the list. Returns NULL if this STMTLIST does not exist.
+	 */
 	public STMTLIST getNextStmt()
 	{
 		return nextStmt;
 	}
 	
+	/**
+	 * public void print()
+	 * 
+	 * Print function. Prints the current stmt and, if the next STMTLIST exists, prints that.
+	 */
 	public void print() 
 	{
 		if(stmt != null)
@@ -50,7 +87,12 @@ public class STMTLIST extends Variable
 		else RobotInterpreter.writeln("parse", "EMPTY STMTLIST");
 	}
 
-	//Validate the stmt and the next, if it exists
+	/**
+	 * public void validate()
+	 * 
+	 * Validation function. If the STMT exists, call its validation function. If the next STMTLIST exists, call its validation function
+	 * 
+	 */
 	public void validate() 
 	{
 		RobotInterpreter.writeln("validate", "Validating STMTLIST");
@@ -61,11 +103,21 @@ public class STMTLIST extends Variable
 		}
 	}
 
-	@Override
+	/**
+	 * public Object execute(Object args[])
+	 * 
+	 * Executes the current stmt. Tries to get a return value.
+	 * We will only ever get a return value if we are in a codeBody. If we do get one, we stop immediately and return the value.
+	 * If we do not get one, then we continue execution until we either get a return value or run out of stmts to execute.
+	 * 
+	 * @param args	any args passed to the body. This should always be null here.
+	 * @return	the return value for the STMTLIST, which will only not be null if the STMT is a return STMT and we are in a codeBody.
+	 * 
+	 */
 	public Object execute(Object args[]) 
 	{
 		Object retVal = null;
-		retVal = stmt.execute(args);
+		retVal = stmt.execute(null);
 		
 		if(retVal != null)
 		{
@@ -73,7 +125,7 @@ public class STMTLIST extends Variable
 		}
 		else if(nextStmt != null)
 		{
-			return nextStmt.execute(args);
+			return nextStmt.execute(null);
 		}
 		else return null;
 	}
--- src/robotinterpreter/variables/Variable.java
+++ src/robotinterpreter/variables/Variable.java
@@ -1,24 +1,79 @@
 package robotinterpreter.variables;
+
+/**
+ * abstract class Variable
+ * 
+ * This class is used by every variable (nonterminal) in the program.
+ * It contains variables used in every nonterminal, getter / setter functions for these vars, and abstract functions for the nonterminals to implement.
+ * 
+ * @author Garret Walliman (gwallima@asu.edu)
+ *
+ */
 public abstract class Variable 
 {
+	//The parent body of the nonterminal. Note that this will only be different from the main body for nonterminals within METHODDEFINE code bodies.
 	protected BODY body;
+	//The actual code containing the code making up the nonterminal
 	protected String code;
+	//The line number on which the code above appears
 	protected int lineNum;
 	
+	/**
+	 * These abstract functions must be implemented by every nonterminal
+	 */
+	
+	/**
+	 * public abstract void print()
+	 * 
+	 * Print is used to print out the parsed contents of the nonterminal, in the style of code.
+	 */
 	public abstract void print();
+	
+	/**
+	 * public abstract void validate()
+	 * 
+	 * Validate is used to ensure that the parsed code conforms to semantic rules. We list which semantic rules we must conform to within the comments of each nonterminal.
+	 * We also perform some linking in this function.
+	 */
 	public abstract void validate();
+	
+	/**
+	 * public abstract Object execute(Object[] args)
+	 * 
+	 * This function is used in the execution of the code. Any functionality associated with the nonterminal must be coded here.
+	 * Note that we provide both an Object return type and an Object array for arguments. We do not know a priori what return / objects are used in execute, and so we keep it general.
+	 * (Note that we only use either of these in rare occasions)
+	 * 
+	 * @param args	an array of objects, whatever arguments must be passed to the nonterminal implementing the function.
+	 * @return	whatever the function returns; null if it returns nothing.
+	 */
 	public abstract Object execute(Object[] args);
 	
+	/**
+	 * public BODY body()
+	 * 
+	 * @return	a reference to the parent body of this nonterminal
+	 */
 	public BODY body()
 	{
 		return body;
 	}
 	
+	/**
+	 * public String code()
+	 * 
+	 * @return	the code stored for this nonterminal
+	 */
 	public String code()
 	{
 		return code;
 	}
 	
+	/**
+	 * public int lineNum()
+	 * 
+	 * @return	the line number this nonterminal appears on
+	 */
 	public int lineNum()
 	{
 		return lineNum;
